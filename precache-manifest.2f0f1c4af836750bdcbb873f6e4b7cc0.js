fd7262522219aea8300b2c315652f98782eef160
green-code-developer <complexcodealgorithm@gmail.com>
2020-10-01 14:27:16 +0530

    Updates

---
+++ precache-manifest.2f0f1c4af836750bdcbb873f6e4b7cc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8d94cd8a209b23e757fb8f5aa8b68a0",
+    "url": "/my-google/index.html"
+  },
+  {
+    "revision": "e8809339008094f5736b",
+    "url": "/my-google/static/css/main.eac35912.chunk.css"
+  },
+  {
+    "revision": "61626330fa868862a84e",
+    "url": "/my-google/static/js/2.8f34b2d0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/my-google/static/js/2.8f34b2d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8809339008094f5736b",
+    "url": "/my-google/static/js/main.d01a4e6f.chunk.js"
+  },
+  {
+    "revision": "4399357a39710be045c0",
+    "url": "/my-google/static/js/runtime-main.b6238826.js"
+  }
+]);
